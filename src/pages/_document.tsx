a6befe145e1799eb9cef6de98036a8aa873f82d2
Muhammad Yasir Ismail <yasir@lmntrix.com>
2025-06-02 11:13:32 +0500

    linting issue fixed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7534279843511977"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
